6ee4dfedab53a519b6f522d27f2157df12142925
MirrgieRiana <tacticsrealize2@gmail.com>
2022-04-21 11:10:00 +0900

    Update Logging.kt

--- src/main/java/mirrg/kotlin/slf4j/hydrogen/Logging.kt
+++ src/main/java/mirrg/kotlin/slf4j/hydrogen/Logging.kt
@@ -3,4 +3,5 @@ package mirrg.kotlin.slf4j.hydrogen
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-fun <T : Any> T.getLogger(clazz: Class<*>? = null): Logger = LoggerFactory.getLogger(clazz ?: javaClass)
+fun <T : Any> T.getLogger() = getLogger(javaClass)
+fun getLogger(clazz: Class<*>): Logger = LoggerFactory.getLogger(clazz)
