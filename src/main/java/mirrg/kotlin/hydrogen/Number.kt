1bb9902ae990e0e3fa91c89132d679fcedcd78d5
MirrgieRiana <tacticsrealize2@gmail.com>
2022-04-25 00:07:07 +0900

    Create Number.kt

---
+++ src/main/java/mirrg/kotlin/hydrogen/Number.kt
@@ -0,0 +1,16 @@
+package mirrg.kotlin.hydrogen
+
+infix fun Byte.atMost(other: Byte) = coerceAtMost(other)
+infix fun Byte.atLeast(other: Byte) = coerceAtLeast(other)
+infix fun Short.atMost(other: Short) = coerceAtMost(other)
+infix fun Short.atLeast(other: Short) = coerceAtLeast(other)
+infix fun Int.atMost(other: Int) = coerceAtMost(other)
+infix fun Int.atLeast(other: Int) = coerceAtLeast(other)
+infix fun Long.atMost(other: Long) = coerceAtMost(other)
+infix fun Long.atLeast(other: Long) = coerceAtLeast(other)
+infix fun Float.atMost(other: Float) = coerceAtMost(other)
+infix fun Float.atLeast(other: Float) = coerceAtLeast(other)
+infix fun Double.atMost(other: Double) = coerceAtMost(other)
+infix fun Double.atLeast(other: Double) = coerceAtLeast(other)
+infix fun <T : Comparable<T>> T.atMost(other: T) = coerceAtMost(other)
+infix fun <T : Comparable<T>> T.atLeast(other: T) = coerceAtLeast(other)
