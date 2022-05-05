00d9a730eb65717e973825de4fa7631ae61863bb
MirrgieRiana <tacticsrealize2@gmail.com>
2022-05-05 16:53:25 +0900

    [add] castOrThrow, castOrNull, or

--- src/main/java/mirrg/kotlin/hydrogen/Lang.kt
+++ src/main/java/mirrg/kotlin/hydrogen/Lang.kt
@@ -3,3 +3,6 @@
 package mirrg.kotlin.hydrogen
 
 inline fun unit(block: () -> Unit) = block()
+inline fun <reified O : Any> Any.castOrThrow() = this as O
+inline fun <reified O : Any> Any.castOrNull() = this as? O
+inline fun <S, I : S, O : S> I?.or(default: () -> O): S = this ?: default()
--- src/test/java/mirrg/kotlin/hydrogen/LangTest.kt
+++ src/test/java/mirrg/kotlin/hydrogen/LangTest.kt
@@ -1,9 +1,20 @@
 package mirrg.kotlin.hydrogen
 
 import junit.framework.Assert.assertEquals
+import junit.framework.Assert.fail
 import org.junit.Test
 
 class LangTest {
+    private inline fun <reified T : Throwable> assertThrow(block: () -> Any) {
+        try {
+            block()
+            fail()
+        } catch (e: Throwable) {
+            if (e is T) return
+            throw e
+        }
+    }
+
     @Suppress("UNUSED_EXPRESSION")
     @Test
     fun unitTest() {
@@ -13,4 +24,22 @@ class LangTest {
 
     @Suppress("unused", "UNUSED_EXPRESSION")
     private fun unitTestMethod1() = unit { 10 }
+
+    @Test
+    fun castOrThrowTest() {
+        assertEquals(10, (10 as Any).castOrThrow<Number>())
+        assertThrow<ClassCastException> { ("abc" as Any).castOrThrow<Number>() }
+    }
+
+    @Test
+    fun castOrNullTest() {
+        assertEquals(10, (10 as Any).castOrNull<Number>())
+        assertEquals(null, ("abc" as Any).castOrNull<Number>())
+    }
+
+    @Test
+    fun orTest() {
+        assertEquals("10", (10 as Int?).or { 20.0 }.toInt().toString())
+        assertEquals("20", (null as Int?).or { 20.0 }.toInt().toString())
+    }
 }
