a2ed3b32f13c1f13f0920106e28504abfde7d4a2
MirrgieRiana <tacticsrealize2@gmail.com>
2022-05-06 23:39:00 +0900

    [add] join

--- src/main/java/mirrg/kotlin/hydrogen/String.kt
+++ src/main/java/mirrg/kotlin/hydrogen/String.kt
@@ -2,6 +2,18 @@
 
 package mirrg.kotlin.hydrogen
 
+
+// toStringを呼び出さないjoin
+
+fun <T : CharSequence> Iterable<T>.join(separator: CharSequence = ", ") = joinToString(separator)
+fun <T : CharSequence> Array<T>.join(separator: CharSequence = ", ") = joinToString(separator)
+fun <T : CharSequence> Sequence<T>.join(separator: CharSequence = ", ") = joinToString(separator)
+
+fun <T> Iterable<T>.join(separator: CharSequence = ", ", transform: (T) -> CharSequence) = joinToString(separator, transform = transform)
+fun <T> Array<T>.join(separator: CharSequence = ", ", transform: (T) -> CharSequence) = joinToString(separator, transform = transform)
+fun <T> Sequence<T>.join(separator: CharSequence = ", ", transform: (T) -> CharSequence) = joinToString(separator, transform = transform)
+
+
 /** 先頭の文字のみを大文字にします。 */
 fun String.toUpperCaseHead() = if (isEmpty()) this else take(1).toUpperCase() + drop(1)
 
--- src/test/java/mirrg/kotlin/hydrogen/StringTest.kt
+++ src/test/java/mirrg/kotlin/hydrogen/StringTest.kt
@@ -5,6 +5,23 @@ import org.junit.Test
 
 class StringTest {
     @Test
+    fun joinTest() {
+        assertEquals("a, b, c", listOf("a", "b", "c").asIterable().join())
+        assertEquals("a, b, c", listOf("a", "b", "c").toTypedArray().join())
+        assertEquals("a, b, c", listOf("a", "b", "c").asSequence().join())
+        assertEquals("a|b|c", listOf("a", "b", "c").asIterable().join("|"))
+        assertEquals("a|b|c", listOf("a", "b", "c").toTypedArray().join("|"))
+        assertEquals("a|b|c", listOf("a", "b", "c").asSequence().join("|"))
+
+        assertEquals("1, 2, 3", listOf(1, 2, 3).asIterable().join { "$it" })
+        assertEquals("1, 2, 3", listOf(1, 2, 3).toTypedArray().join { "$it" })
+        assertEquals("1, 2, 3", listOf(1, 2, 3).asSequence().join { "$it" })
+        assertEquals("1|2|3", listOf(1, 2, 3).asIterable().join("|") { "$it" })
+        assertEquals("1|2|3", listOf(1, 2, 3).toTypedArray().join("|") { "$it" })
+        assertEquals("1|2|3", listOf(1, 2, 3).asSequence().join("|") { "$it" })
+    }
+
+    @Test
     fun to_CaseHeadTest() {
         assertEquals("AbcAbc", "abcAbc".toUpperCaseHead())
         assertEquals("AbcAbc", "AbcAbc".toUpperCaseHead())
