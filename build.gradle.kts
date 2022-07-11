226e72ce6f55784fa2cc33d051fbb4b135ef189a
MirrgieRiana <tacticsrealize2@gmail.com>
2022-07-11 23:54:12 +0900

    revert:

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.slf4j:slf4j-api:1.7.36")
-    testImplementation("org.apache.logging.log4j:log4j-core:2.17.2")
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("com.google.code.gson:gson:2.9.0")
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.2")
+    implementation("junit:junit:4.13.2")
+    implementation("com.google.code.gson:gson:2.9.0")
 }
