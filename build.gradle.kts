2732d04e04af204a9ffe8fced2f4afd3f20214b9
Kouda-Titanium <tacticsrealize2@gmail.com>
2023-11-04 04:20:36 +0900

    build: clean

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,6 @@ plugins {
     kotlin("jvm") version "1.5.20"
 }
 
-tasks.withType<JavaCompile> {
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
-    options.encoding = "UTF-8"
-}
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
