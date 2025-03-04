0f8f2b1d471deed901aabab3165d1c266e9cd23d
_ <tacticsrealize2@gmail.com>
2025-03-04 09:12:20 +0900

    Update Logging.kt

--- src/main/java/mirrg/kotlin/slf4j/hydrogen/Logging.kt
+++ src/main/java/mirrg/kotlin/slf4j/hydrogen/Logging.kt
@@ -24,3 +24,4 @@ import org.slf4j.LoggerFactory
 fun <T : Any> T.getLogger() = getLogger(javaClass)
 fun getLogger(name: String): Logger = LoggerFactory.getLogger(name)
 fun getLogger(clazz: Class<*>): Logger = LoggerFactory.getLogger(clazz)
+fun getFileLogger(topLevelAnonymousObject: Any) = getLogger(topLevelAnonymousObject::class.java.enclosingClass!!)
