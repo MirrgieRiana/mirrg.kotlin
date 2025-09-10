c6691bc1696c9a1d7bd716a2da65747c53e4c907
_ <tacticsrealize2@gmail.com>
2025-09-10 15:30:15 +0900

    ADD: Lang: contract

--- src/commonMain/kotlin/mirrg/kotlin/helium/Lang.kt
+++ src/commonMain/kotlin/mirrg/kotlin/helium/Lang.kt
@@ -1,12 +1,40 @@
 @file:Suppress("unused", "SpellCheckingInspection")
+@file:OptIn(ExperimentalContracts::class)
 
 package mirrg.kotlin.helium
 
-inline fun unit(block: () -> Unit) = block()
-inline fun <reified O : Any> Any?.castOr(default: () -> O) = this as? O ?: default()
-inline fun <reified O : Any> Any?.castOrThrow() = this as O
-inline fun <reified O : Any> Any?.castOrNull() = this as? O
-inline fun <S> S?.or(default: () -> S) = this ?: default()
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+inline fun unit(block: () -> Unit) {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+    block()
+}
+
+inline fun <reified O : Any> Any?.castOr(default: () -> O): O {
+    contract {
+        callsInPlace(default, InvocationKind.AT_MOST_ONCE)
+    }
+    return this as? O ?: default()
+}
+
+inline fun <reified O : Any> Any?.castOrThrow(): O {
+    return this as O
+}
+
+inline fun <reified O : Any> Any?.castOrNull(): O? {
+    return this as? O
+}
+
+inline fun <S> S?.or(default: () -> S): S {
+    contract {
+        callsInPlace(default, InvocationKind.AT_MOST_ONCE)
+    }
+    return this ?: default()
+}
 
 fun Boolean.toUnitOrNull() = if (this) Unit else null
 
